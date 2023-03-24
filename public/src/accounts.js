84db6e8c69b160566caa8e4ccd55dfb7232a4b96
Brandon Duarte <115111346+BZDLearning@users.noreply.github.com>
2023-03-23 23:10:21 -0700

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,5 +1,10 @@
 function findAccountById(accounts, id) {
-  return accounts.find(account => account.id === id);
+  return findById(accounts, id);
+}
+
+//Helper function for the above function. ^^
+function findById(bookStock, id) {
+  return bookStock.find(book => book.id === id);
 }
 
 function sortAccountsByLastName(accounts) {
